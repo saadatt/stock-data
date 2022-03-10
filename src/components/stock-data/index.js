d618241420a935160a17a1dacf3f7603a3c8d03e
Sadat <saadatt@gmail.com>
2022-03-10 12:31:46 -0900

    updated index.js

--- src/components/stock-data/index.js
+++ src/components/stock-data/index.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import "./index.css";
 
 export default function StockData() {
-  return (
-    <div className="layout-column align-items-center mt-50">
-      <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="5-January-2000" id="app-input" data-testid="app-input"/>
-        <button className="" id="submit-button" data-testid="submit-button">Search</button>
-      </section>
-      <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
-        <li className="py-10"></li>
-      </ul>
-      <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result"></div>
-    </div>
-  );
+
+    const [input, setInput] = useState();
+    const [stoks, setStoks] = useState();
+
+    const onClickSearch = async () => {
+        try {
+            const response = await fetch('https://jsonmock.hackerrank.com/api/stocks?date=' + input)
+            const data = await response.json();
+            console.log('first', data);
+            if (data.total) {
+                setStoks(data.data);
+            } else {
+                setStoks([]);
+            }
+        } catch (err) {
+            setStoks([]);
+        }
+    }
+
+    return (
+        <div className="layout-column align-items-center mt-50">
+            <section className="layout-row align-items-center justify-content-center">
+                <input
+                    type="text" className="large" placeholder="5-January-2000"
+                    id="app-input" data-testid="app-input"
+                    onChange={e => setInput(e.target.value)}
+                />
+                <button className="" id="submit-button" data-testid="submit-button" onClick={onClickSearch}>
+                    Search
+                </button>
+            </section>
+            {stoks
+                ? stoks.length > 0
+                    ? stoks.map((stock, index) =>
+                        <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data" key={index}>
+                            <li className="py-10">
+                                Open: {stock.open}
+                            </li>
+                            <li className="py-10">
+                                Close: {stock.close}
+                            </li>
+                            <li className="py-10">
+                                High: {stock.high}
+                            </li>
+                            <li className="py-10">
+                                Low: {stock.low}
+                            </li>
+                        </ul>)
+                    : <div data-testid="no-result">No Results Found</div>
+                : null}
+        </div>
+    );
 }
