28c72e7baddfd6369cf052015d92046f48912ddb
saadatt <saadatt@gmail.com>
2022-03-11 09:19:25 -0600

    added build files

---
+++ build/precache-manifest.21d55a62fd57d2c4f854c2e1cc7dc77e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f0965892985f81d881a0dfc84cd28b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de57e296ed2747b0a61e",
+    "url": "/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "3dcf92432e44ac72a336",
+    "url": "/static/css/main.faf8c058.chunk.css"
+  },
+  {
+    "revision": "de57e296ed2747b0a61e",
+    "url": "/static/js/4.04d31eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/4.04d31eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e19e7b4e928c64e6ae",
+    "url": "/static/js/5.b5166012.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/5.b5166012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c8cf56cf659b7f3b4d",
+    "url": "/static/js/6.9f8711cd.chunk.js"
+  },
+  {
+    "revision": "3dcf92432e44ac72a336",
+    "url": "/static/js/main.8b1d016e.chunk.js"
+  },
+  {
+    "revision": "a75e14c611014074fc43",
+    "url": "/static/js/runtime-main.618226f8.js"
+  },
+  {
+    "revision": "48398df9ed4dc1d802cf",
+    "url": "/static/js/stencil-h8k-logo_2-entry-js.33fa60fa.chunk.js"
+  },
+  {
+    "revision": "ab8a4932f3cc5fa1dd01",
+    "url": "/static/js/stencil-polyfills-css-shim.b1cc621a.chunk.js"
+  }
+]);
